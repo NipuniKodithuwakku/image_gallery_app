4e297447d24a9a4ef9739bc8091f92cdc2d7d81f
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-03-07 07:47:50 -0800

    add http get request

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,32 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
 
-class App extends StatefulWidget{
-  createState(){
+class App extends StatefulWidget {
+  createState() {
     return AppState();
   }
 }
 
-class AppState extends State<App>{
+class AppState extends State<App> {
   int counter = 0;
-  
-  Widget build(context){
+  void fetchImage() async {
+    counter++;
+   var response = await get('https://jsonplaceholder.typicode.com/photos/$counter',);
+   var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+  }
+
+  Widget build(context) {
     return MaterialApp(
-    home: Scaffold(
-      appBar : AppBar(
-        title:Text('image gallery'),
-      ),
-      body: Text('$counter'),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-         setState((){
-           counter++;
-         });
-        },
-        child:Icon (Icons.add),
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('image gallery'),
         ),
-    ),
-
-  );
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
